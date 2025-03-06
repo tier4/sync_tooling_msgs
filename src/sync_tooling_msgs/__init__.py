ec9e4729a098adea163db574743f9c50206639e3
Max SCHMELLER <max.schmeller@tier4.jp>
2025-03-06 15:20:50 +0900

    feat: make ClockId, PortId hashable
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

---
+++ src/sync_tooling_msgs/__init__.py
@@ -0,0 +1,21 @@
+from google.protobuf.message import Message
+from .clock_id_pb2 import ClockId
+from .port_id_pb2 import PortId
+
+
+def _proto_hash(message: Message):
+    """Hash a given proto message stably. The hash is only valid during runtime.
+
+    See https://protobuf.dev/programming-guides/serialization-not-canonical/#deterministic-is-not-canonical
+    for details on why hashes cannot reliably be persisted across executions.
+
+    Args:
+        message (Message): The message to hash
+
+    Returns:
+        int: The hash value
+    """
+    return hash(message.SerializePartialToString(deterministic=True))
+
+ClockId.__hash__ = _proto_hash
+PortId.__hash__ = _proto_hash
---
+++ tests/hashing_test.py
@@ -0,0 +1,44 @@
+from sync_tooling_msgs.clock_id_pb2 import ClockId
+from sync_tooling_msgs.frame_id_pb2 import FrameId
+from sync_tooling_msgs.port_id_pb2 import PortId
+
+clock_id_test1 = ClockId(frame_id=FrameId(frame="test"))
+clock_id_test2 = ClockId(frame_id=FrameId(frame="test"))
+clock_id_other = ClockId(frame_id=FrameId(frame="other"))
+clock_id_empty = ClockId()
+
+port_id_domain0 = PortId(clock_id=clock_id_test1, port_number=1, ptp_domain=0)
+port_id_domain1 = PortId(clock_id=clock_id_test1, port_number=1, ptp_domain=1)
+
+def test_clock_id_hash_basics():
+  assert clock_id_test1 == clock_id_test2
+  assert clock_id_test1 != clock_id_other
+  assert clock_id_test1 != clock_id_empty
+  assert hash(clock_id_test1) == hash(clock_id_test2)
+
+  # Not true for all possible clock IDs (hashes can clash), but likely (and deterministic).
+  assert hash(clock_id_test1) != hash(clock_id_other)
+
+def test_port_id_hash_basics():
+  assert port_id_domain0 != port_id_domain1
+  assert hash(port_id_domain0) != hash(port_id_domain1)
+
+def test_clock_id_collections():
+  mapping = {
+    clock_id_test1: "a",
+    clock_id_other: "b",
+    clock_id_empty: "c"
+  }
+
+  assert len(mapping) == 3
+  assert mapping[clock_id_test1] == "a"
+  assert mapping[clock_id_test2] == "a"
+
+def test_port_id_collection():
+  mapping = {
+    port_id_domain0: "a",
+    port_id_domain1: "b"
+  }
+
+  assert len(mapping) == 2
+  assert mapping[port_id_domain0] == "a"
