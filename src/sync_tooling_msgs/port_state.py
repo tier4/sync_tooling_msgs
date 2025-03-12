683b5420ef6a61179b067a4567ab380e6cef4d4e
Max SCHMELLER <max.schmeller@tier4.jp>
2025-03-12 22:07:46 +0900

    chore: make PortState diagnosable
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- src/sync_tooling_msgs/port_state.py
+++ src/sync_tooling_msgs/port_state.py
@@ -1,22 +1,57 @@
 import bidict
-from sync_tooling_msgs.port_state_pb2 import PortState as ps
 
-PORT_STATE_NAMES = bidict.bidict({
-  ps.PS_UNKNOWN: "NONE",
-	ps.PS_INITIALIZING: "INITIALIZING",
-	ps.PS_FAULTY: "FAULTY",
-	ps.PS_DISABLED: "DISABLED",
-	ps.PS_LISTENING: "LISTENING",
-	ps.PS_PRE_MASTER: "PRE_MASTER",
-	ps.PS_MASTER: "MASTER",
-	ps.PS_PASSIVE: "PASSIVE",
-	ps.PS_UNCALIBRATED: "UNCALIBRATED",
-	ps.PS_SLAVE: "SLAVE",
-	ps.PS_GRAND_MASTER: "GRAND_MASTER",
-})
+from sync_tooling_msgs.diag_tree import to_diag_tree
+from sync_tooling_msgs.error_pb2 import Error
+from sync_tooling_msgs.ok_pb2 import Ok
+from sync_tooling_msgs.port_state_pb2 import PortState as PS
+from sync_tooling_msgs.warning_pb2 import Warning
 
-def port_state_name(state: ps.ValueType) -> str:
-  return PORT_STATE_NAMES[state]
+PORT_STATE_NAMES = bidict.bidict(
+    {
+        PS.PS_UNKNOWN: "NONE",
+        PS.PS_INITIALIZING: "INITIALIZING",
+        PS.PS_FAULTY: "FAULTY",
+        PS.PS_DISABLED: "DISABLED",
+        PS.PS_LISTENING: "LISTENING",
+        PS.PS_PRE_MASTER: "PRE_MASTER",
+        PS.PS_MASTER: "MASTER",
+        PS.PS_PASSIVE: "PASSIVE",
+        PS.PS_UNCALIBRATED: "UNCALIBRATED",
+        PS.PS_SLAVE: "SLAVE",
+        PS.PS_GRAND_MASTER: "GRAND_MASTER",
+    }
+)
 
-def port_state_value(name: str) -> ps.ValueType:
-  return PORT_STATE_NAMES.inverse[name]
+
+def port_state_name(state: PS.ValueType) -> str:
+    return PORT_STATE_NAMES[state]
+
+
+def port_state_value(name: str) -> PS.ValueType:
+    return PORT_STATE_NAMES.inverse[name]
+
+
+def diagnose_port_state(state: PS.ValueType):
+    match state:
+        case PS.PS_UNKNOWN:
+            return to_diag_tree(Error(msg="Invalid port state"))
+        case PS.PS_MASTER | PS.PS_SLAVE | PS.PS_GRAND_MASTER:
+            return to_diag_tree(
+                Ok(msg=f"Port is operating nominally ({port_state_name(state)})")
+            )
+        case (
+            PS.PS_LISTENING | PS.PS_INITIALIZING | PS.PS_PRE_MASTER | PS.PS_UNCALIBRATED
+        ):
+            return to_diag_tree(
+                Warning(msg=f"Port is in a transient state ({port_state_name(state)})")
+            )
+        case PS.PS_DISABLED:
+            return to_diag_tree(
+                Warning(
+                    msg=f"Port exists but is not being used ({port_state_name(state)})"
+                )
+            )
+        case _:
+            return to_diag_tree(
+                Error(msg=f"Port is not working correctly ({port_state_name(state)})")
+            )
