141c3bf53e74a56310b3d8f4122ae95f5a12a2e8
Max SCHMELLER <max.schmeller@tier4.jp>
2025-03-12 22:08:06 +0900

    chore: add option to print PortId without domain
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- src/sync_tooling_msgs/port_id.py
+++ src/sync_tooling_msgs/port_id.py
@@ -2,6 +2,8 @@ from sync_tooling_msgs.clock_id import readable_clock_id
 from sync_tooling_msgs.port_id_pb2 import PortId
 
 
-PortKey = str
-def readable_port_id(port_id: PortId) -> str:
-    return f"{port_id.ptp_domain}:{readable_clock_id(port_id.clock_id)}-{port_id.port_number}"
+def readable_port_id(port_id: PortId, include_domain = True) -> str:
+    readable_id = f"{readable_clock_id(port_id.clock_id)}-{port_id.port_number}"
+    if (include_domain):
+        readable_id = f"{port_id.ptp_domain}:{readable_id}"
+    return readable_id
