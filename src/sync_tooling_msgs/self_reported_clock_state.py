45d1a5ac90f4b5e7939ad8b087f843ce699b292a
Max SCHMELLER <max.schmeller@tier4.jp>
2025-04-16 11:16:31 +0900

    feat: add self reported clock state graph update type
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

---
+++ src/sync_tooling_msgs/self_reported_clock_state.py
@@ -0,0 +1,70 @@
+"""Utility functions for self-reported clock states"""
+
+import bidict
+
+from sync_tooling_msgs.diag_tree import to_diag_tree
+from sync_tooling_msgs.diag_tree_pb2 import DiagTree
+from sync_tooling_msgs.error_pb2 import Error
+from sync_tooling_msgs.ok_pb2 import Ok
+from sync_tooling_msgs.self_reported_clock_state_update_pb2 import (
+    SelfReportedClockStateUpdate,
+)
+from sync_tooling_msgs.warning_pb2 import Warning
+
+CLOCK_STATE_NAMES = bidict.bidict(
+    {
+        SelfReportedClockStateUpdate.State.INVALID: "INVALID",
+        SelfReportedClockStateUpdate.State.UNSYNCHRONIZED: "UNSYNCHRONIZED",
+        SelfReportedClockStateUpdate.State.TRACKING: "TRACKING",
+        SelfReportedClockStateUpdate.State.LOCKED: "LOCKED",
+        SelfReportedClockStateUpdate.State.LOST: "LOST",
+    }
+)
+
+
+def clock_state_name(
+    state: SelfReportedClockStateUpdate.State.ValueType,
+) -> str:
+    """Return the canonical name for the given clock state enum value"""
+    return CLOCK_STATE_NAMES[state]
+
+
+def clock_state_value(
+    name: str,
+) -> SelfReportedClockStateUpdate.State.ValueType:
+    """Return the enum value for the given canonical clock state name"""
+    return CLOCK_STATE_NAMES.inverse[name]
+
+
+def diagnose_clock_state(
+    state: SelfReportedClockStateUpdate.State.ValueType,
+) -> DiagTree:
+    """
+    Diagnose a given clock state enum value.
+
+    For valid states, the diagnostic tree is of type `Ok`. For transient states
+    and invalid states, the diagnostic tree is of type `Error`.
+
+    Specifically, the state is only `Ok` if the clock state is `LOCKED` and `Warning` if the
+    clock state is `TRACKING`. Otherwise, the state is `Error`.
+
+    Args:
+        state: The clock state enum value to diagnose
+
+    Returns:
+        The diagnostic tree for the given clock state. This is always a tree of type `Ok` or
+        `Error`.
+    """
+    match state:
+        case SelfReportedClockStateUpdate.State.INVALID:
+            return to_diag_tree(Error(msg="Invalid clock state"))
+        case SelfReportedClockStateUpdate.State.UNSYNCHRONIZED:
+            return to_diag_tree(Error(msg="Clock is not synchronized"))
+        case SelfReportedClockStateUpdate.State.TRACKING:
+            return to_diag_tree(Warning(msg="Clock is tracking"))
+        case SelfReportedClockStateUpdate.State.LOCKED:
+            return to_diag_tree(Ok(msg="Clock is locked"))
+        case SelfReportedClockStateUpdate.State.LOST:
+            return to_diag_tree(Error(msg="Clock lost synchronization"))
+        case _:
+            raise AssertionError(f"Unknown clock state: {state}")
