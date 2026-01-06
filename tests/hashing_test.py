a7c9069294cde8910fa759cff5ad4d15f005d1e4
Max SCHMELLER <max.schmeller@tier4.jp>
2026-01-06 12:03:53 +0900

    chore: add license headers everywhere
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- tests/hashing_test.py
+++ tests/hashing_test.py
@@ -1,3 +1,17 @@
+# Copyright 2025 TIER IV, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 def test_clock_id_hash_basics(
     clock_id_test1, clock_id_test2, clock_id_other, clock_id_empty
 ):
